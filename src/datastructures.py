19a565d993e7230a90e2c9bda8c2180f4bcd4569
AlbaReverte <albareverteruiz@gmail.com>
2024-12-10 18:25:20 +0000

    Alba Reverte Family Jackson

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,23 +13,46 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+        "id": self._generateId(),
+        "first_name": "John",
+        "last_name": last_name,
+        "age": 33,
+        "lucky_numbers": [7, 13, 22]
+        },{
+        "id": self._generateId(),
+        "first_name": "Jane",
+        "last_name": last_name,
+        "age": 35,
+        "lucky_numbers": [10, 14, 3]
+        },{
+        "id": self._generateId(),
+        "first_name": "Jimmy",
+        "last_name": last_name,
+        "age": 5,
+        "lucky_numbers": 1
+        }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
+        # éste es el método POST
         # fill this method and update the return
-        pass
+        self._members.append(member)
+        return member
+        
 
     def delete_member(self, id):
         # fill this method and update the return
+        self._members = [member for member in self._members if member['id'] != id]
         pass
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        return next((member for member in self._members if member['id'] == id), None)
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
